73285683a04a3f0008650a1d36c9c7e92a431c7b
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2024-01-29 19:22:37 +0300

    alefa

--- Admin/src/Card.jsx
+++ Admin/src/Card.jsx
@@ -8,19 +8,19 @@ import './assets/fontawesome-5/css/all.min.css';
 import './assets/fontawesome-5/css/all.css';
 
 
-export default function Card({annonce,idvoiture,prix,date, image, lieu, descs}) {
+export default function Card({annonce,idvoiture,prix,date, sary, lieu, descs}) {
     const datePart = date.split('T')[0];
     return (
         <div className='col-md-4 mb-4'>
             <div className="card">
-                <img src={image} alt='image de vehicule' className='card-img-top img-fluid rounded' />
+                <img src={sary} alt='image de vehicule' className='card-img-top img-fluid rounded' />
                 <div className='py-2' />
                 <div className='card-body'>
                     <h5 className='card-title h3'>Prix :{prix}Ar</h5>
                     <p className='card-text'>Date annonce: {datePart}</p>
                     <p className='card-text'>Description: {descs}</p>
                     <p className='card-text'>lieu: {lieu}</p>
-                   <Link to={`/detail?idVoiture=${encodeURIComponent(idvoiture)}&prix=${encodeURIComponent(prix)}&annonce=${encodeURIComponent(annonce)}&image=${encodeURIComponent(image)}`}> <button className='btn btn-success btn-lg' >details annonce</button></Link>
+                   <Link to={`/detail?idVoiture=${encodeURIComponent(idvoiture)}&prix=${encodeURIComponent(prix)}&annonce=${encodeURIComponent(annonce)}&image=${encodeURIComponent(sary)}`}> <button className='btn btn-success btn-lg' >details annonce</button></Link>
                 </div>
             </div>
         </div>
